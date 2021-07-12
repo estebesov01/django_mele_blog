2f0d03e22a08f7122e47bfecf80a63d173f2bc79
estebesov01 <estebesov7777@gmail.com>
2021-07-12 15:17:13 +0600

    blog_tags

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.conf import settings
 from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -13,7 +14,6 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
-
 
 
 def post_list(request, tag_slug=None):
@@ -53,11 +53,17 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
